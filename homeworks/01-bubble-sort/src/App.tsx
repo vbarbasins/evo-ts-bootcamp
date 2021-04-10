21a3aea60d97d3a0798d8bfa4c76c26394b21384
vbarbasins <vbarbasins@evolution.com>
2021-04-10 16:24:41 +0300

    01: Add bubble sorting using state

--- homeworks/01-bubble-sort/src/App.tsx
+++ homeworks/01-bubble-sort/src/App.tsx
@@ -8,6 +8,8 @@ interface AppProps {}
 interface AppState {
   sorting: boolean,
   numbers: number[],
+  lastI: number,
+  lastJ: number,
 }
 
 class App extends React.Component<{}, AppState> {
@@ -16,6 +18,8 @@ class App extends React.Component<{}, AppState> {
     this.state = {
       sorting: false,
       numbers: createRandomNumberArray(100, 30),
+      lastI: 0,
+      lastJ: 0,
     };
   }
 
@@ -23,15 +27,43 @@ class App extends React.Component<{}, AppState> {
     this.setState({
       sorting: false,
       numbers: createRandomNumberArray(100, 30),
+      lastI: 0,
+      lastJ: 0,
     });
   }
 
   progressHandler = () => {
     const { sorting } = this.state;
     if (!sorting) {
-      this.setState({ sorting: true });
+      this.setState((prevState) => ({
+        ...prevState,
+        sorting: true,
+      }));
+      setInterval(this.sortNumbers, 100);
     } else {
-      this.setState({ sorting: false });
+      this.setState((prevState) => ({
+        ...prevState,
+        sorting: false,
+      }));
+    }
+  }
+
+  sortNumbers = () => {
+    const { numbers, lastI, lastJ } = this.state;
+    if (lastI < numbers.length) {
+      const newNumbers = [...numbers];
+      const nextJ = lastJ + 1;
+      if (numbers[lastJ] > numbers[nextJ]) {
+        const tmp = newNumbers[lastJ];
+        newNumbers[lastJ] = newNumbers[nextJ];
+        newNumbers[nextJ] = tmp;
+      }
+      this.setState((prevState) => ({
+        ...prevState,
+        numbers: newNumbers,
+        lastI: nextJ < numbers.length - 1 ? prevState.lastI : prevState.lastI + 1,
+        lastJ: nextJ < numbers.length - 1 ? nextJ : 0,
+      }));
     }
   }
 
@@ -45,7 +77,7 @@ class App extends React.Component<{}, AppState> {
         <button onClick={this.progressHandler}>
           {sorting ? 'Pause' : 'Start'}
         </button>
-        <p>{numbers}</p>
+        <p>{`numbers: ${numbers.toString()}`}</p>
       </div>
     );
   }
