a79e8e23fab2850667d6a24bd89709c0957376a4
vbarbasins <vbarbasins@evolution.com>
2021-05-02 23:49:30 +0300

    05: Add and cleaup CRA project, add eslint

---
+++ homeworks/05-redux-basic/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface AppProps {}
+
+interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+ 
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
