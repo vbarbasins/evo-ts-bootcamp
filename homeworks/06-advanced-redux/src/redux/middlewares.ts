f4fcf4ba0847924157f6827c7fe2230f7ff12960
vbarbasins <vbarbasins@evolution.com>
2021-05-17 15:09:18 +0300

    06: Use 'switch' instead of 'if' in logger conditions

--- homeworks/06-advanced-redux/src/redux/middlewares.ts
+++ homeworks/06-advanced-redux/src/redux/middlewares.ts
@@ -8,16 +8,19 @@ import { LogEvent, ShopState } from '../types';
 
 export const logger: Middleware<{}, ShopState> = ({ getState }) => (nextDispatch) => (
   (action: AnyAction) => {
-    let event: LogEvent = { eventName: action.type };
-    const shopActionTypes = [
-      ShopActionType.ItemSelected,
-      ShopActionType.ItemAddedToCart,
-      ShopActionType.ItemRemovedFromCart,
-    ];
-    if (shopActionTypes.includes(action.type)) {
-      const itemId = action.payload;
-      const { name, price } = getState().menu.filter((item) => item._id === itemId)[0];
-      event = { ...event, itemName: name, itemPrice: price };
+    let event: LogEvent;
+    switch (action.type) {
+      case ShopActionType.ItemSelected:
+      case ShopActionType.ItemAddedToCart:
+      case ShopActionType.ItemRemovedFromCart: {
+        const itemId = action.payload;
+        const { name, price } = getState().menu.filter((item) => item._id === itemId)[0];
+        event = { eventName: action.type, itemName: name, itemPrice: price };
+        break;
+      }
+      default: {
+        event = { eventName: action.type };
+      }
     }
     sendLogs(event);
     return nextDispatch(action);
