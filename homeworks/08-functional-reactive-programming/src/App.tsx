f8f83ecfa821227ce4ad126262c06f1ba6835df8
vbarbasins <vbarbasins@evolution.com>
2021-05-22 15:41:52 +0300

    08: Add initial grid

--- homeworks/08-functional-reactive-programming/src/App.tsx
+++ homeworks/08-functional-reactive-programming/src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
 
-export const App: React.FC = () => (
-  <div/>
-);
+const GRID_CELL_COUNT = 100;
+
+export const App: React.FC = () => {
+  const cells = [];
+  for (let i = 0; i < GRID_CELL_COUNT; i += 1) {
+    cells.push(<div className="cell" key={i}>x</div>);
+  }
+  return (
+    <div className="grid">
+      {cells}
+    </div>
+  );
+};
