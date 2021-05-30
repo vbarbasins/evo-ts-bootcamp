cf9028088f46d7fcd8728ac287f9e761297b979c
vbarbasins <vbarbasins@evolution.com>
2021-05-30 21:21:28 +0300

    08: Avoid imperative code in game grid generation

--- homeworks/08-functional-reactive-programming/src/utils.ts
+++ homeworks/08-functional-reactive-programming/src/utils.ts
@@ -1,16 +1,13 @@
 import { GRID_CELL_COUNT, INITIAL_WINDOWS } from './consts';
 import { Cell, CellContent } from './types';
 
-export const generateGrid = (): Cell[] => {
-  const cells: Cell[] = [];
-  for (let i = 0; i < GRID_CELL_COUNT; i += 1) {
-    cells.push({
-      id: i,
-      content: INITIAL_WINDOWS.includes(i) ? CellContent.WINDOW : CellContent.WALL,
-    });
-  }
-  return cells;
-};
+export const generateGrid = (): Cell[] => Array.from(
+  { length: GRID_CELL_COUNT },
+  (v, n) => ({
+    id: n,
+    content: INITIAL_WINDOWS.includes(n) ? CellContent.WINDOW : CellContent.WALL,
+  }),
+);
 
 export const getRandomTarget = (): number => (
   INITIAL_WINDOWS[Math.floor(Math.random() * INITIAL_WINDOWS.length)]
