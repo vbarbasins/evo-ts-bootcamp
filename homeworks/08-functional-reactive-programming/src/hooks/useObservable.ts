06842d9ece9f65ec025c93979adeabcec0fb0c5d
vbarbasins <vbarbasins@evolution.com>
2021-07-06 09:15:02 +0300

    08: Remove useless subsription

--- homeworks/08-functional-reactive-programming/src/hooks/useObservable.ts
+++ homeworks/08-functional-reactive-programming/src/hooks/useObservable.ts
@@ -1,16 +1,13 @@
-import { useState, useEffect, useMemo } from 'react';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { useState, useEffect } from 'react';
+import { Observable } from 'rxjs';
 
 export const useObservable = <T>(observable: Observable<T>, initial: T): T => {
   const [value, setValue] = useState<T>(initial);
-  const subject = useMemo(() => new Subject(), []);
 
   useEffect(() => {
-    const subscription = new Subscription();
-    subscription.add(subject);
-    subscription.add(subject.pipe(() => observable).subscribe((subValue) => setValue(subValue)));
+    const subscription = observable.subscribe(setValue);
     return () => subscription.unsubscribe();
-  }, [subject]);
+  }, [observable]);
 
   return value;
 };
